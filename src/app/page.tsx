9899fbefb6ddd922b3f4de160ff820b4f4013daa
PedroHenrique1606 <pedromelosilva11911@gmail.com>
2024-06-02 21:24:45 -0300

    feat: navbar and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Navbar from "@/components/Navbar/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-slate-50 h-screen">
+      <nav>
+        <Navbar />
+      </nav>
     </main>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,37 @@
-export default function Navbar(){
-    
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-between px-10 py-10 text-black">
+      <div className="flex items-center">
+        <a href="" className="text-2xl font-semibold text-blue-400">
+          Traveling!
+        </a>
+      </div>
+
+      <div className="flex-1 flex justify-center">
+        <ul className="flex gap-20">
+          <li>
+            <a href="" className="hover:text-blue-500">
+              Product
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:text-blue-500">
+              Contacts
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:text-blue-500">
+              About Us
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex items-center">
+        <button className="px-5 py-3 bg-orange-500 text-white rounded-lg font-medium hover:bg-orange-600">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
 }
